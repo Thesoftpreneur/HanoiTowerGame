23665c3643b788cea46d19ac14a9274a2c8c7e34
Marcel Seremak <marcelseremak47@gmail.com>
2024-02-15 15:39:45 +0100

    Dodanie przeniesienia na tryb Game, wyrysowanie kijków, na których będą dyski oraz dodanie tekstów z nazwami poziomów do każdego poziomu trudności

--- main.py
+++ main.py
@@ -2,22 +2,59 @@ import sys
 from os import environ
 environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 from button import Button
-
 import pygame
 
 pygame.init()
 background_color = (128, 229, 175)
 window_size = (900, 500)
+screen = pygame.display.set_mode(window_size)
 game_font = pygame.font.Font("Freedom-10eM.ttf", 100)
 button_font = pygame.font.Font("Freedom-10eM.ttf", 30)
+level_font = pygame.font.Font("Freedom-10eM.ttf", 50)
 
 
-def game(discs=4):
-    pass
+def game(discs, level):
+    pygame.display.set_caption("Hanoi Tower / Game")
+    pygame.display.flip()
+
+    while True:
+
+        bg = pygame.image.load("background.png")
+        screen.blit(bg, (0, 0))
+        if level == "easy":
+            level_text = level_font.render("Level: Easy", True, (255, 255, 255))
+            shadow_level_text = level_font.render("Level: Easy", True, (0, 0, 0))
+            screen.blit(shadow_level_text, (window_size[0] // 2 - level_text.get_width() // 2 - 7, 60))
+            screen.blit(level_text, (window_size[0] // 2 - level_text.get_width() // 2, 60))
+
+        elif level == "medium":
+            level_text = level_font.render("Level: Medium", True, (255, 255, 255))
+            shadow_level_text = level_font.render("Level: Medium", True, (0, 0, 0))
+            screen.blit(shadow_level_text, (window_size[0] // 2 - level_text.get_width() // 2 - 7, 60))
+            screen.blit(level_text, (window_size[0] // 2 - level_text.get_width() // 2, 60))
+
+        else:
+            level_text = level_font.render("Level: Hard", True, (255, 255, 255))
+            shadow_level_text = level_font.render("Level: Hard", True, (0, 0, 0))
+            screen.blit(shadow_level_text, (window_size[0] // 2 - level_text.get_width() // 2 - 7, 60))
+            screen.blit(level_text, (window_size[0] // 2 - level_text.get_width() // 2, 60))
+
+        pygame.draw.rect(screen, (88, 66, 29), (100, 400, 200, 25))
+        pygame.draw.rect(screen, (88, 66, 29), (350, 400, 200, 25))
+        pygame.draw.rect(screen, (88, 66, 29), (600, 400, 200, 25))
+        pygame.draw.rect(screen, (88, 66, 29), (190, 200, 20, 200))
+        pygame.draw.rect(screen, (88, 66, 29), (440, 200, 20, 200))
+        pygame.draw.rect(screen, (88, 66, 29), (690, 200, 20, 200))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        pygame.display.flip()
+
 
 def mainMenu():
     pygame.display.set_caption("Hanoi Tower / Menu")
-    screen = pygame.display.set_mode(window_size)
     pygame.display.flip()
 
 
@@ -46,7 +83,16 @@ def mainMenu():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif easy_button.is_clicked(event):
+                game(discs=4, level="easy")
+
+            elif medium_button.is_clicked(event):
+                game(discs=6, level="medium")
+
+            elif hard_button.is_clicked(event):
+                game(discs=7, level="hard")
 
         pygame.display.flip()
+
 mainMenu()
 
